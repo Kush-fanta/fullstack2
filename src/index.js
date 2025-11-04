6265a56e386fd93496b223afcb27d609a1d47a93
Kushagra Vyas <vyaskushagra56@gmail.com>
2025-11-04 17:33:32 +0530

    updated files and folders

---
+++ src/index.js
@@ -0,0 +1,24 @@
+require('dotenv').config({path: './env'})
+import mongoose from 'mongoose';
+import { DB_NAME } from './constants';
+import express from 'express';
+
+const app = express()
+
+;( async ()=>{
+    try{
+        const connection_instance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`\nMongoDB  Connection Sccuessfull, DB HOST: ${connection_instance}`)
+        app.on("error", (error) => {
+            console.error("Error: ",error)
+            throw error;
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log(`Server is running on port ${process.env.PORT}`);
+        })
+    } catch(error){
+        console.error("Error: ",error)
+        throw error;
+    }
+})()
